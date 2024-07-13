fe5cf7cb47dd5c466af524e9a54db62b2fdd1174
OriginalSin <serg@auchat.ru>
2024-07-13 19:40:12 +0300

    up

--- boris/db/authTele.js
+++ boris/db/authTele.js
@@ -1,18 +1,8 @@
-requestAnimationFrame(()=> {
-    console.warn('ggg', window.mtNs)
-
-})
-window.mtNsCallBack = (data) => {
-    console.warn('mtNsCallBack', data);
-
-};
-
 window.addEventListener('load', (e) => {
     // console.warn('doc', e, document.body)
     const body = document.body;
     const clList = body.classList;
     const form = document.forms[0];
-    // const origin = location.hostname;   // host с которого авторизуемся
     const hostId = 21;                  // host с которого авторизуемся
     const pType = 15;                   // номер страницы с которой авторизуемся
     const bName = 'phistory1_bot';      // имя бота
@@ -27,10 +17,7 @@ window.addEventListener('load', (e) => {
                 pType,
                 (1000 + Math.random() * 9000).toFixed(0)
             ].join('_');
-            // window.mtNsCallBack = (data) => {
-            //     console.warn('mtNsCallBack', key, data);
 
-            // };
             const ow = mtNs.Utils.getWindowOpen(
                 `https://moretele.ru/auth/index.html?botName=${bName}&start=${key}`,
                 {
@@ -40,11 +27,9 @@ window.addEventListener('load', (e) => {
             const owClose = (flag) => {
                 if (!ow.closed) ow.close();
                 if (flag) clList.remove('mtSessMask');
-
             };
 
             window.addEventListener('beforeunload', () => {
-                debugger
                 owClose(true);
             });
 
@@ -55,24 +40,18 @@ window.addEventListener('load', (e) => {
                         if (v.bot && v.bot.name === bName) {
                             const {text, from} = v.message;
                             if (key === text.substring(7)) {
-                                console.warn('vvvv', key, from)
+                                // console.warn('vvvv', key, from)
                                 owClose(true);
-                                sessionStorage.setItem('mtSess', JSON.stringify(from));
-
+                                const tgUser = JSON.stringify(from);
+                                sessionStorage.setItem('mtSess', tgUser);
+                                const tgUserInput = document.createElement('input', tgUser);
+                                form.append(tgUserInput);
                             }
-
                         }
                     })
                 }
-                // console.warn('message', key, ow, e.data)
                 if (ow.closed) owClose(true);
-
             });
-        
-
         }
-        // console.warn('submit', ev, document.body)
-
     });
-    // debugger
 });
