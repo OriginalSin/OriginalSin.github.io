dfaa3eabba4989e1ffa094f790480e510cc39d4a
OriginalSin <serg@auchat.ru>
2024-07-13 16:15:52 +0300

    up

--- boris/db/authTele.js
+++ boris/db/authTele.js
@@ -7,20 +7,30 @@ window.addEventListener('load', (e) => {
     const body = document.body;
     const clList = body.classList;
     const form = document.forms[0];
+    const origin = location.origin; // host с которого авторизуемся
+    const pType = 15;               // номер страницы с которой авторизуемся
     
     form.addEventListener('submit', async (ev) => {
         let mtSess = sessionStorage.getItem('mtSess');
         if (!mtSess) {
             ev.preventDefault();
             clList.add('mtSessMask');
-            const key = (1000 + Math.random() * 9000).toFixed(0);
+            let key = [
+                origin,
+                pType,
+                (1000 + Math.random() * 9000).toFixed(0)
+            ].join(':');
 
-            const tme = `https://t.me/phistory1_bot?start=${key}`;
-            const bqr = await mtNs.Utils.getQrImgSrc(tme);
+            const url = `https://t.me/phistory1_bot?start=${key}`;
+            const bqr = await mtNs.Utils.getQrImgSrc({url});
             body.querySelector('img.qrCode').src = bqr;
-            body.querySelector('a.qrLink').href = tme;
+            body.querySelector('a.qrLink').href = url;
+            mtNs.bcc.addEventListener('message', e => {
+                const gg = e.data;
+                console.warn('submit', key, gg)
+
+            });
         
-            // console.warn('submit', tme, bqr)
 
         }
         // console.warn('submit', ev, document.body)
